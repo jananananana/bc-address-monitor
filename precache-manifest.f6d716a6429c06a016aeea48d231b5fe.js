9a8eb1147bfc1d0c4ca18f0ce45defe98b60f7ba
Jana Veliskova <jveliskova@rjmetrics.com>
2019-04-08 13:09:17 +0200

    Updates

---
+++ precache-manifest.f6d716a6429c06a016aeea48d231b5fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19500f29827433c7052e",
+    "url": "/bc-address-monitor/static/js/runtime~main.d5192b32.js"
+  },
+  {
+    "revision": "ecfaa0d6b73a1e111dae",
+    "url": "/bc-address-monitor/static/js/main.8faf03f9.chunk.js"
+  },
+  {
+    "revision": "7685573bbbdc0ba85f9a",
+    "url": "/bc-address-monitor/static/js/2.2a3879e5.chunk.js"
+  },
+  {
+    "revision": "ecfaa0d6b73a1e111dae",
+    "url": "/bc-address-monitor/static/css/main.cc9167a6.chunk.css"
+  },
+  {
+    "revision": "410425751cdff730c8fa982b11adedf8",
+    "url": "/bc-address-monitor/index.html"
+  }
+];
