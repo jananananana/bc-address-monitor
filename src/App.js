52bc336a3401b632849bd216d11ba33e221ec6a6
Jana Veliskova <jveliskova@rjmetrics.com>
2019-04-21 20:28:59 -0400

    update things

--- src/App.js
+++ src/App.js
@@ -4,45 +4,14 @@ import './App.css';
 import Ledger from './components/Ledger';
 import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
-import ParseMessage from "./services/ParseMessage.js";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    // Save the messages from the server in messages, and then used to render
-    this.state = { messages: [] };
-  }
-
-  // Move this into ledger
-  componentDidMount() {
-    // Create a new socket connection with relative URL
-    const sock = new WebSocket('wss://ws.blockchain.info/inv');
-
-    sock.onopen = () => {
-      console.log('open');
-      this.setState({ actions: sock });
-    };
-
-    sock.onmessage = (e) => {
-      console.log('message', e.data);
-      // messages from server, store in state
-      const transactions = ParseMessage.extractTransactionInfo(e.data);
-
-      this.setState({ messages: [...this.state.messages, transactions] });
-    };
-
-    sock.onclose = () => console.log('close');
-
-    sock.onerror = e => console.error(e.message);
-
-  }
 
   render() {
     return (
       <div className="App">
         <AppHeader/>
-        <Ledger {... this.state} />
+        <Ledger />
         <AppFooter/>
       </div>
     );
--- src/components/Ledger.js
+++ src/components/Ledger.js
@@ -1,26 +1,103 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ParseMessage from "./../services/ParseMessage.js";
 
 class Ledger extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Save the messages from the server in messages, and then used to render
+    this.state = { messages: [] };
+  }
+
+  // Move this into ledger
+  componentDidMount() {
+    // Create a new socket connection with relative URL
+    const sock = new WebSocket('wss://ws.blockchain.info/inv');
+
+    sock.onopen = () => {
+      console.log('open');
+      this.setState({ actions: sock });
+    };
+
+    sock.onmessage = (e) => {
+      console.log('message', e.data);
+      // messages from server, store in state
+      const transactions = ParseMessage.extractTransactionInfo(e.data);
+
+      this.setState({ messages: [...this.state.messages, transactions] });
+    };
+
+    sock.onclose = () => console.log('close');
+
+    sock.onerror = e => console.error(e.message);
+
+  }
+
+  fetchAddressData (address) {
+    const fetchOptions = {
+            method: 'GET',
+            headers: new Headers({
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*://*/*",
+              "Access-Control-Allow-Methods": "GET, POST",
+              "Access-Control-Allow-Headers": "Content-Type"
+            }),
+            mode: 'no-cors'
+          };
+
+    fetch(`https://blockchain.info/rawaddr/${address}&cors=true?callback=foo`, fetchOptions)
+      .then (function(response) {
+        if (!response.ok) {
+          throw new Error('HTTP error, status = ' + response.status)
+        } else {
+          function foo(response) {
+            console.log(response);
+          }
+        }
+      })
+
+    // fetch('https://blockchain.info/rawaddr/'+address,{
+    //     method: 'GET',
+    //     mode: 'no-cors',
+    //     referrer: 'no-referral',
+    //     headers: {
+    //       'Content-Type':'application/json',
+    //       'Access-Control-Allow-Origin': '*',
+    //     }
+    //   })
+    // .then(res => res)
+    // .then(
+    //     (result) => console.log("result: ", result),
+    //     (error) => {
+    //       console.log('error');
+    //       console.log(error);
+    //     }
+    // );
+
+
+  }
+
   formHandler (e) {
     e.preventDefault();
-    const text = this.refs.addressInput.value;
-    this.props.actions.send(
+    const addr = this.refs.addressInput.value;
+    this.state.actions.send(
       JSON.stringify(
         //{"op":"unconfirmed_sub"}
         { 
           op: "addr_sub",
-          addr: `${text}`
+          addr,
         }
       )
     );
-    // Use fetch to get the data from the API
+    this.fetchAddressData(addr);
     // Put the most recent transactions in the messages
   } 
 
   render() {
     let i = 0,
-      messages = this.props.messages.map(message => {
+      messages = this.state.messages.map(message => {
         return <li key={i++}>{message}</li>
       });
 
